5be5f572270bbdf5b37a66ff27cbf25d00b4b903
Dilyara7 <dilarasajmerden04@gmail.com>
2024-10-17 16:26:53 +0500

    ContactForm.vue

--- src/components/ContactForm.vue
+++ src/components/ContactForm.vue
@@ -14,7 +14,7 @@
           />
         </div>
   
-        <div class="form-group">
+        <div class="form-group"></div>
           <label for="age">Возраст:</label>
           <input
             type="number"
